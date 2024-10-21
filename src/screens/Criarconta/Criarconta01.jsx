8d2be9567a96f61384307d6de650b3b0a4526ea1
Natan20.sys <eusuez@gmail.com>
2024-10-20 23:25:41 -0300

    update

---
+++ src/screens/Criarconta/Criarconta01.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, ImageBackground, TouchableOpacity, SafeAreaView} from "react-native";
+import { LinearGradient } from 'expo-linear-gradient';
+import { estilo } from "./style";
+import { Image } from "expo-image";
+
+export const Criarconta01 = () => {
+    return(
+        <View style={estilo.container}>
+            <ImageBackground 
+            source={'../../../assets/WhatsApp Image 2024-08-25 at 16.11.54.jpeg'}
+            resizeMode="cover" 
+            style={estilo.background}> 
+                <View>
+                    <Text style={estilo.titulo}>APLICATIVO {'\n'}BADBONS</Text>
+                </View>
+                <SafeAreaView style={estilo.container2}>
+                    <LinearGradient 
+                    start={{x: 0, y: 0}} 
+                    end={{x: 1, y: 0}} 
+                    colors={['#4376e6', '#0233a1']} 
+                    style={estilo.linearGradientButton}
+                    >
+                        <TouchableOpacity style={estilo.button}>
+                            <Text style={estilo.buttonText}>Inicie seus treinos</Text>
+                        </TouchableOpacity>
+                    </LinearGradient>
+                </SafeAreaView>
+            </ImageBackground>
+        </View>
+    );
+}
---
+++ src/screens/Perfil/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ScrollView, View, Text} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { estilo } from "../style";
+
+export default function PerfilScreen() {
+    return (
+        <ScrollView contentContainerStyle={estilo.container}>
+            <Text style={estilo.font}>Perfil</Text>
+        </ScrollView>
+    );
+}
